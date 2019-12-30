269e6fd7a4878a95039584a0a574ed122b8d1701
oleg <oleg_mnv@yahoo.com>
2019-12-30 18:09:14 +0300

    Insert configuration in the main flow

--- counter/code/main.py
+++ counter/code/main.py
@@ -7,21 +7,21 @@ def count_incedents():
     """Главный механизм, выполнен в виде генератора для экономии памяти.
         :yield: расчетные данные по каждому значению в файле
     """
-    # TODO: insert path to CSV file. Config data from command line.
-    dT = 0.3
     # Getting configuration of programm
-    # conf = get_config()
+    conf = get_config()
+
     # Prepare CSV writer coroutine
-    writer = csv_writer()
+    writer = csv_writer(conf['of'])
+
     # Working with data
-    for sample in csv_reader():
+    for sample in csv_reader(conf['if']):
         counter = 0
-        for data in csv_reader():
+        for data in csv_reader(conf['if']):
             # Same ID
             if data[0] == sample[0]:
                 continue
             # Not valid time
-            if not compare_time(dT, sample[3], data[3]):
+            if not compare_time(conf['dt'], sample[3], data[3]):
                 continue
             # Not matched values
             if not compare_values(sample, data):
@@ -30,11 +30,10 @@ def count_incedents():
             counter += 1
         # Write result to file
         writer.send([ sample[0], counter])
+
     # Close writer coroutine
     writer.close()
 
 
 if __name__ == "__main__":
-    # count_incedents()
-
-    print(get_config())
+    count_incedents()
