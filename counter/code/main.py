e7ab3ab87c8d447b0ef1b45ae58162e44422cae3
oleg <oleg_mnv@yahoo.com>
2019-12-30 16:43:37 +0300

    Full flow of programm!!!

--- counter/code/main.py
+++ counter/code/main.py
@@ -1,18 +1,35 @@
 from file_handlers import csv_reader, csv_writer
-from tools import get_config
+from tools import get_config, compare_time, compare_values
 
 
-def count_incedents(conf):
+def count_incedents(conf, dT=0.3):
     """Главный механизм, обрабатывающий все события программы.
     :return: ничего, заставляет работать других
     """
     # TODO: insert path to CSV file. Config data from command line.
-    reader = csv_reader()
-    for data in reader:
-        print(f"{data}\n")
+    res = []
+    for sample in csv_reader():
+        res.append({
+            'id': sample[0],
+            'count': 0
+        })
+        for data in csv_reader():
+            # Same ID
+            if data[0] == res[-1]['id']:
+                continue
+            # Not valid time
+            if not compare_time(dT, sample[3], data[3]):
+                continue
+            # Not matched values
+            if not compare_values(sample, data):
+                continue
+            res[-1]['count'] += 1
+    return res
 
 
 if __name__ == "__main__":
     conf = get_config()
+    res = count_incedents(conf)
 
-    count_incedents(conf)
+    for val in res:
+        print(val)
--- counter/code/tools.py
+++ counter/code/tools.py
@@ -3,3 +3,11 @@ def get_config():
     :return: словарь с конфигурационными данными
     """
     return {}
+
+
+def compare_time(dT:float, t1:float, t2:float):
+    return 0 < (t1-t2) <= dT
+
+
+def compare_values(d1, d2):
+    return d1[1] == d2[1] and d1[2] == d2[2]
