6f5fd1f9fd92335e9f4fc842be36e7d0c410c3d9
oleg <oleg_mnv@yahoo.com>
2019-12-30 14:25:47 +0300

    Code of Data Generator

--- data_generator/code/main.py
+++ data_generator/code/main.py
@@ -1,1 +1,20 @@
-print("Hello there!!!")
+# Libraries
+import numpy as np
+import pandas as pd
+
+
+# Configuration
+M = 2
+N = 10
+
+
+# Data Frame
+df = pd.DataFrame({
+    'feature1': np.random.randint(M, size=(N,)),
+    'feature2': np.random.randint(M, size=(N,)),
+    'time': np.random.rand(N)
+})
+
+
+# Save data to CSV-file
+df.to_csv('/tmp/incidents.csv', index_label='id')
