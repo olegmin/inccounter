9c725694ed790343efc207af0232b79b9e4bc301
oleg <oleg_mnv@yahoo.com>
2019-12-31 13:40:36 +0300

    Fix field with time, add STDOUT status

--- data_generator/code/main.py
+++ data_generator/code/main.py
@@ -9,15 +9,22 @@ def generate():
     conf = get_config()
     # Prepare file writer
     writer = csv_writer(conf['output_file'])
-
+    # Print output file
+    print(f"Write data in file: {conf['output_file']}")
+    # Generate data
+    counter = 0
     for i in range(conf['N']):
+        # Write to file new line with data
         writer.send([
             i,
             np.random.randint(conf['M']),
             np.random.randint(conf['M']),
-            np.random.randn(conf['N'])
+            np.random.uniform(0.01, 1.0)
         ])
-
+        # Count itterations and print status
+        counter += 1
+        if counter % 1000 == 0:
+            print(f"\t- Write {counter} / {conf['N']} lines.")
     # Close writer coroutine
     writer.close()
 
