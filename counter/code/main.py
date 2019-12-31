e0e0677b837b0ae9d0ca4e383c01d5609f432acd
oleg <oleg_mnv@yahoo.com>
2019-12-31 21:17:19 +0300

    WIP: Insert pandas, refactor code to this library

--- counter/code/main.py
+++ counter/code/main.py
@@ -1,7 +1,7 @@
 from file_handlers import csv_reader, csv_writer
 from tools import get_config, print_config, timer
 from compares import compare_time, compare_values
-
+import pandas as pd
 
 @timer
 def count_incedents():
@@ -10,36 +10,37 @@ def count_incedents():
     """
     # Getting configuration of programm
     conf = get_config()
-    print_config(conf)
 
-    # Prepare CSV writer coroutine
-    writer = csv_writer(conf['output_file'])
+    # # Prepare CSV writer coroutine
+    # writer = csv_writer(conf['output_file'])
 
     # Reading all data
-    all_data = []
-    for sample in csv_reader(conf['input_file']):
-        all_data.append(sample)
+    all_data = pd.read_csv(conf['input_file'], sep=';')
+    print(all_data)
 
-    # Working with data
-    for sample in all_data:
-        counter = 0
-        for data in all_data:
-            # Same ID
-            if data[0] == sample[0]:
-                continue
-            # Not valid time
-            if not compare_time(conf['delta_time'], sample[3], data[3]):
-                continue
-            # Not matched values
-            if not compare_values(sample, data):
-                continue
-            # Increment counter
-            counter += 1
-        # Write results to file
-        writer.send([sample[0], counter])
+    res = all_data.groupby(['feature1', 'feature2'])['id'].count()
+    print(res)
 
-    # Close writer coroutine
-    writer.close()
+    # # Working with data
+    # for sample in all_data:
+    #     counter = 0
+    #     for data in all_data:
+    #         # Same ID
+    #         if data[0] == sample[0]:
+    #             continue
+    #         # Not valid time
+    #         if not compare_time(conf['delta_time'], sample[3], data[3]):
+    #             continue
+    #         # Not matched values
+    #         if not compare_values(sample, data):
+    #             continue
+    #         # Increment counter
+    #         counter += 1
+    #     # Write results to file
+    #     writer.send([sample[0], counter])
+    #
+    # # Close writer coroutine
+    # writer.close()
 
 
 if __name__ == "__main__":
