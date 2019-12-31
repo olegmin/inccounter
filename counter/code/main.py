ef9891dcbaeec1030795a35f9f2d84c65b965546
oleg <oleg_mnv@yahoo.com>
2019-12-31 21:29:13 +0300

    WIP: Correct reading data from CSV-file

--- counter/code/main.py
+++ counter/code/main.py
@@ -11,36 +11,10 @@ def count_incedents():
     # Getting configuration of programm
     conf = get_config()
 
-    # # Prepare CSV writer coroutine
-    # writer = csv_writer(conf['output_file'])
-
     # Reading all data
-    all_data = pd.read_csv(conf['input_file'], sep=';')
+    all_data = pd.read_csv(conf['input_file'], sep=';', usecols=['feature1', 'feature2', 'time'])
+    # Test data
     print(all_data)
-
-    res = all_data.groupby(['feature1', 'feature2'])['id'].count()
-    print(res)
-
-    # # Working with data
-    # for sample in all_data:
-    #     counter = 0
-    #     for data in all_data:
-    #         # Same ID
-    #         if data[0] == sample[0]:
-    #             continue
-    #         # Not valid time
-    #         if not compare_time(conf['delta_time'], sample[3], data[3]):
-    #             continue
-    #         # Not matched values
-    #         if not compare_values(sample, data):
-    #             continue
-    #         # Increment counter
-    #         counter += 1
-    #     # Write results to file
-    #     writer.send([sample[0], counter])
-    #
-    # # Close writer coroutine
-    # writer.close()
 
 
 if __name__ == "__main__":
