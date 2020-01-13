9e706e8eb72ef28f20b68c647fdbeb06e4ab5434
oleg <oleg_mnv@yahoo.com>
2020-01-13 03:13:11 +0300

    New version of code

--- counter/code/main.py
+++ counter/code/main.py
@@ -1,7 +1,6 @@
-from file_handlers import csv_reader, csv_writer
-from tools import get_config, print_config, timer
-from compares import compare_time, compare_values
+from tools import get_config, timer
 import pandas as pd
+
 
 @timer
 def count_incedents():
@@ -12,9 +11,34 @@ def count_incedents():
     conf = get_config()
 
     # Reading all data
-    all_data = pd.read_csv(conf['input_file'], sep=';', usecols=['feature1', 'feature2', 'time'])
-    # Test data
-    print(all_data)
+    all_data = pd.read_csv(conf['input_file'], sep=';', index_col='id')
+
+    # Calculate incedents
+    dt = conf['delta_time']
+    ci = []
+    for index, row in all_data.iterrows():
+        # Fields of curent incedents
+        f1 = int(row['feature1'])
+        f2 = int(row['feature2'])
+        t = row['time']
+        # Search valid incedents
+        valid_incedents = all_data.query("feature1 == @f1 & feature2 == @f2 & @t-time>0 & @t-time<=@dt")
+        # Count incedents
+        if valid_incedents.empty:
+            lenght = 0
+        else:
+            lenght, _ = valid_incedents.shape
+        # Append number of incedents
+        ci.append([lenght])
+        # TODO: delete
+        if index % 1000 == 0:
+            print(f"Calc {index} values")
+
+    # Create DataFrame with counts
+    cincd = pd.DataFrame(ci, columns=['counts'])
+
+    # Save counted incedents to output file
+    cincd.to_csv(conf['output_file'], index_label='id')
 
 
 if __name__ == "__main__":
